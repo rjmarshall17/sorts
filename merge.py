bcebd82c7ca33a6df698deb3b880a2a5342ca90f
Rob Marshall <rob.marshall17@gmail.com>
2021-01-03 22:24:29 -0500

    Change x = x + 1 to x += 1 add sorts to pseudocode

--- merge.py
+++ merge.py
@@ -19,21 +19,21 @@ def mergeSort(arr):
         while i < len(left_half) and j < len(right_half):
             if left_half[i] <= right_half[j]:
                 arr[k] = left_half[i]
-                i = i + 1
+                i += 1
             else:
                 arr[k] = right_half[j]
-                j = j + 1
-            k = k + 1
+                j += 1
+            k += 1
 
         while i < len(left_half):
             arr[k] = left_half[i]
-            i = i + 1
-            k = k + 1
+            i += 1
+            k += 1
 
         while j < len(right_half):
             arr[k] = right_half[j]
-            j = j + 1
-            k = k + 1
+            j += 1
+            k += 1
     # print("Merging ",arr)
 
 
